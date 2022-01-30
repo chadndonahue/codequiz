3ab1ee2f1a148f7872c3e46a74ff7729ad45bf41
Chad <chadndonahue@gmail.com>
2022-01-30 15:56:46 -0500

    made and array of questions

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,48 @@
-const question = document.querySelector("#question")
+const question = document.querySelector("#question");
 const choices = Array.from(document.querySelectorAll(".choice-text"));
-const progressText = document.querySelector("#progressText")
-const scoreText = document.querySelector("#score")
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "What is going on",
+        choice1: "Cao ni ma",
+        choice2: "Sha Bi",
+        choice3: "DA jiba",
+        choice4: "idk dog i dont speak mandarin",
+    },
+    {
+        question: "What is going on",
+        choice1: "Cao ni ma",
+        choice2: "Sha Bi",
+        choice3: "DA jiba",
+        choice4: "idk dog i dont speak mandarin",
+    },
+    {
+        question: "What is going on",
+        choice1: "Cao ni ma",
+        choice2: "Sha Bi",
+        choice3: "DA jiba",
+        choice4: "idk dog i dont speak mandarin",
+    },
+    {
+        question: "What is going on",
+        choice1: "Cao ni ma",
+        choice2: "Sha Bi",
+        choice3: "DA jiba",
+        choice4: "idk dog i dont speak mandarin",
+    },
+    {
+        question: "What is going on",
+        choice1: "Cao ni ma",
+        choice2: "Sha Bi",
+        choice3: "DA jiba",
+        choice4: "idk dog i dont speak mandarin",
+    },
+]
