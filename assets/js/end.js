72fd4c9ce41e11d43527b9404ff839f0db7014aa
Chad <chadndonahue@gmail.com>
2022-01-30 18:57:59 -0500

    finished endgame page and i believe the js is working for it

---
+++ assets/js/end.js
@@ -0,0 +1,25 @@
+const username = document.querySelector("#name");
+const scoreBtn = document.querySelector("#scoreBtn");
+const finalScore = document.querySelector("#finalScore");
+const recentScore = document.querySelector("#recentScore")
+
+const scores = json.parse(localStorage.getItem("scores")) || []
+
+const MAX_SCORES = 3
+
+finalScore.innerText = recentScore
+
+saveScore = i => {
+    i.preventDefault()
+    const score = {
+        score: recentScore,
+        name: username.value
+    }
+    scores.push(score)
+    scores.sort((a,b) => {
+        return b.score-a.score
+    })
+    scores.splice(3)
+    localStorage.setItem("scores", JSON.stringify(scores))
+    window.location.assign("/")
+}
